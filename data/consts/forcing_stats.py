6e8ccffb6ad9c7500adb2c9192c9209916d486bc
Roshni Biswas <rbiswas@ds.dfci.harvard.edu>
2020-08-11 08:38:41 -0400

    add data/consts

---
+++ data/consts/forcing_stats.py
@@ -0,0 +1,35 @@
+# Statistics of Relative Humidity calculated in the fwi_forcings.ipynb
+# Calculated on 12 months of data without applying the mask
+FORCING_MEAN_RH = 72.47605
+FORCING_MAD_RH = 13.0844555
+FORCING_VAR_RH = 314.80286
+FORCING_STD_RH = 17.7426847
+FORCING_MIN_RH = 0.0013500393
+FORCING_MAX_RH = 100.064285
+
+# Statistics of T2 calculated in the fwi_forcings.ipynb
+# Calculated on 12 months of data without applying the mask
+FORCING_MEAN_T2 = 279.96622
+FORCING_MAD_T2 = 16.938044
+FORCING_VAR_T2 = 452.84903
+FORCING_STD_T2 = 21.2802498
+FORCING_MIN_T2 = 193.66562
+FORCING_MAX_T2 = 324.16202
+
+# Statistics of TP calculated in the fwi_forcings.ipynb
+# Calculated on 12 months of data without applying the mask
+FORCING_MEAN_TP = 2.4548044
+FORCING_MAD_TP = 3.0407052
+FORCING_VAR_TP = 40.771793
+FORCING_STD_TP = 6.3852794
+FORCING_MIN_TP = 0.0
+FORCING_MAX_TP = 1097.1971
+
+# Statistics of Wind Speed calculated in the fwi_forcings.ipynb
+# Calculated on 12 months of data without applying the mask
+FORCING_MEAN_WSPEED = 6.4765906
+FORCING_MAD_WSPEED = 2.936887
+FORCING_VAR_WSPEED = 13.666987
+FORCING_STD_WSPEED = 3.69688883
+FORCING_MIN_WSPEED = 5.722046e-05
+FORCING_MAX_WSPEED = 47.25583
---
+++ data/consts/fwi_reanalysis_stats.py
@@ -0,0 +1,11 @@
+# Statistics of FWI reanalysis calculated on 12 month of data demonstrated
+# in the fwi_reanalysis.ipynb notebook. Calculated without applying the mask
+REANALYSIS_FWI_MEAN = 15.292629
+REANALYSIS_FWI_MAD = 18.819166
+REANALYSIS_FWI_VAR = 621.65894
+REANALYSIS_FWI_MIN = 1.7216163e-09
+REANALYSIS_FWI_MAX = 304.7236
+
+# The variance of FWI reanalysis calculated after applying the
+# box-cox transformation
+PROCESSED_REANALYSIS_FWI_VAR = 10.813191
