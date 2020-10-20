507569339f043a5230c7556c2ac2c8424fcdb6d3
lazyoracle <contact@anuragsaharoy.me>
2020-10-20 11:40:25 +0000

    add plotting

--- src/plot.py
+++ src/plot.py
@@ -1,5 +1,70 @@
 import json
-from pprint import pprint
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+from argparse import Namespace
+import warnings
+
+
+def str2num(s):
+    """
+    Converts parameter strings to appropriate types.
+
+    :param s: Parameter value
+    :type s: str
+    :return: Appropriately converted value
+    :rtype: Varying
+    """
+    if isinstance(s, bool):
+        return s
+    s = str(s)
+    if "," in s:
+        return [str2num(i) for i in s.split(",")]
+    if "." in s or "e-" in s:
+        try:
+            return float(s)
+        except:
+            pass
+    elif s.isdigit():
+        return int(s)
+    elif s.lower() == "inf":
+        return float("inf")
+    elif s.lower() == "none":
+        return None
+    else:
+        if s.lower() == "true":
+            return True
+        elif s.lower() == "false":
+            return False
+    return s
+
+
+def get_hparams(
+    in_days: ("Number of input days [int]", "option") = 2,
+    out_days: ("Number of output days [int]", "option") = 1,
+    binned: (
+        "Show the extended metrics for supplied comma separated binned FWI value range "
+        "(e.g. 0,15,70) [Bool/list]",
+        "option",
+    ) = "0,5.2,11.2,21.3,38.0,50",
+    case_study: (
+        "The case-study region to use for inference: australia, california, portugal,"
+        " siberia, chile, uk [Bool/str]",
+        "option",
+    ) = False,
+    benchmark: (
+        "Benchmark the FWI-Forecast data against FWI-Reanalysis [Bool]",
+        "option",
+    ) = False,
+):
+    """
+    Process and print the dictionary of project wide arguments.
+
+    :return: Dictionary containing configuration options.
+    :rtype: dict
+    """
+    config_dict = {k: str2num(v) for k, v in locals().items()}
+    return config_dict
 
 
 def read_json(results_file: str) -> tuple:
@@ -8,6 +73,8 @@ def read_json(results_file: str) -> tuple:
 
     :param results_file: path to json results
     :type results_file: str
+    :return: binned metrics and results summary
+    :rtype: tuple
     """
     with open(results_file, "r") as res_file:
         # the results file is a json where each record
@@ -55,6 +122,8 @@ def make_dict(metric_list_dict: dict) -> dict:
 
     :param metric_list_dict: dict of list of dicts
     :type metric_list_dict: dict
+    :return: dict of dict of dicts
+    :rtype: dict
     """
 
     # Use any one of acc, mae, mse to get bins
@@ -82,6 +151,214 @@ def make_dict(metric_list_dict: dict) -> dict:
     return binned_metrics
 
 
-metric_list_dict, results_summary = read_json("results.json")
+def single_day_plot(result, hparams, m, benchmark=None):
+    """
+    Plot mteric results for single day output.
 
-pprint(metric_list_dict)
+    :param result: Model inference result dictionary
+    :type result: dict
+    :param hparams: Hyperparamters
+    :type hparams: Namespace
+    :param benchmark: Benchmark result dictionary, defaults to None
+    :type benchmark: dict, optional
+    :param m: metric
+    :type m: str
+    """
+    bin_range = hparams.binned
+    bin_labels = [
+        f"({bin_range[i]}, {bin_range[i+1]}]"
+        for i in range(len(bin_range))
+        if i < len(bin_range) - 1
+    ]
+    bin_labels.append(f"({bin_range[-1]}, inf)")
+
+    fwi_levels = ["V. Low", "Low", "Mod", "High", "V. High", "Extreme"]
+    bin_labels = [bin_labels[i] + "\n" + fwi_levels[i] for i in range(len(bin_labels))]
+
+    xlab = "FWI Level"
+    # The label locations
+    x = np.arange(len(bin_labels))
+    # The width of the bars
+    width = 0.7 / (3 if benchmark else 2)
+    title = f"{hparams.in_days} Day Input // 1 Day Prediction (Global)"
+    fig, ax = plt.subplots()
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ylabel = {
+        "acc": "Accuracy",
+        "mae": "Mean absolute error",
+        "mse": "Mean squared error",
+    }
+    ax.set_ylabel(ylabel[m])
+    ax.set_xlabel(xlab)
+    ax.set_title(title)
+    ax.set_xticks(x)
+    ax.set_xticklabels(bin_labels)
+
+    num_groups = 2 if benchmark else 1
+
+    rect_list = []
+    preds = [x[0] for x in result.values()]
+    rect_list.append(
+        ax.bar(
+            x - width * num_groups / 2 + width * 1 / 2, preds, width, label="deepFWI"
+        )
+    )
+    if benchmark:
+        bench = [x[0] for x in benchmark.values()]
+        rect_list.append(
+            ax.bar(
+                x - width * num_groups / 2 + width * 1 / 2 + width * 1,
+                bench,
+                width,
+                label="FWI-Forecast",
+            )
+        )
+
+    # bar labels not needed, since it is not legible
+    #     for rect in rect_list:
+    #         autolabel(rect, ax, width)
+
+    if benchmark:
+        ax.legend()
+
+    fig.tight_layout()
+    file_name = time.strftime("%Y%m%d-%H%M%S")
+
+    plt.savefig(file_name, dpi=600, bbox_inches="tight")
+
+
+def multi_day_plot(result, hparams, benchmark=None, m="acc"):
+    """
+    Plot mteric results for single day output.
+
+    :param result: Model inference result dictionary
+    :type result: dict
+    :param hparams: Hyperparamters
+    :type hparams: Namespace
+    :param benchmark: Benchmark result dictionary, defaults to None
+    :type benchmark: dict, optional
+    :param m: metric
+    :type m: str, optional
+    """
+    bin_range = hparams.binned
+
+    fwi_levels = ["V. Low", "Low", "Mod", "High", "V. High", "Extreme"]
+
+    bin_labels = [
+        f"({bin_range[i]}, {bin_range[i+1]}]"
+        for i in range(len(bin_range))
+        if i < len(bin_range) - 1
+    ]
+    bin_labels.append(f"({bin_range[-1]}, inf)")
+
+    bin_labels = [bin_labels[i] + " " + fwi_levels[i] for i in range(len(bin_labels))]
+
+    labels = list(range(len(list(result.values())[0])))
+
+    width = 1 / (len(bin_range) + 1)
+    x = np.arange(len(list(result.values())[0]))
+
+    preds = [list(x.values()) for x in result.values()]
+
+    fig, ax = plt.subplots()
+    rects = []
+
+    color = ["#008000", "#FFFF00", "#FFA500", "#FF0000", "#654321", "#000000"]
+    for i in range(len(bin_range)):
+        rects.append(
+            ax.bar(
+                x - width * (len(bin_range) + 1) / 2 + (i + 1) * width,
+                [x for x in preds[i]],
+                width,
+                label=bin_labels[i],
+                align="center",
+                color=color[i],
+            )
+        )
+
+    if benchmark:
+        bench = [list(x.values()) for x in benchmark.values()]
+        for i in range(len(bin_range)):
+            ax.plot(
+                x,
+                bench[i],
+                color=color[i],
+                marker="o",
+                zorder=3,
+                label=None if i else r"$\mathbf{FWI-Forecast}$",
+            )
+
+    ax.plot([], [], " ", label=r"$\mathbf{deepFWI}$")
+
+    ylabel = {
+        "acc": "Accuracy in %",
+        "mae": "Mean absolute error (A.U.)",
+        "mse": "Mean squared error (A.U.)",
+    }
+    ax.set_ylabel(ylabel[m])
+    ax.set_title(
+        f"{hparams.in_days} Day Input // {hparams.out_days} Day Prediction "
+        f"({hparams.case_study if hparams.case_study else 'Global'})"
+    )
+
+    ax.set_xticks(x)
+    ax.set_xlabel("Day of FWI Prediction")
+    ax.set_xticklabels(labels)
+    ax.legend()
+    handles, labels = ax.get_legend_handles_labels()
+    handles.append("_")
+    labels.append("FWI-forecast")
+    ax.legend(
+        handles, labels, bbox_to_anchor=(1, 1), loc="upper left", fontsize="x-small"
+    )
+
+    # bar labels not needed, since it is not legible
+    #     for rect in rects:
+    #         autolabel(rect, ax, width)
+
+    fig.tight_layout()
+
+    file_name = time.strftime("%Y%m%d-%H%M%S")
+
+    plt.savefig(file_name, dpi=600, bbox_inches="tight")
+
+
+def plot(**kwargs) -> None:
+    """
+    Make plots. The kwargs are equivalent to commandline args
+    """
+    plt.rcParams["figure.figsize"] = [20, 10]
+    plt.rcParams["font.size"] = 18
+    plt.rcParams["legend.fontsize"] = "large"
+    plt.rcParams["figure.titlesize"] = "medium"
+
+    warnings.filterwarnings("ignore")
+
+    hparams = Namespace(**get_hparams(**kwargs))
+
+    result, results_summary = read_json("results.json")
+
+    for m in ["acc", "mse", "mae"]:
+        if hparams.out_days > 1:
+            multi_day_plot(
+                result=result[m],
+                hparams=hparams,
+                m=m,
+                benchmark=None,
+            )
+        else:
+            single_day_plot(
+                result=result[m],
+                hparams=hparams,
+                m=m,
+                benchmark=None,
+            )
+
+
+config = dict(
+    in_days=4,
+    out_days=14,
+)
+
+plot(**config, benchmark=False)
